17abfd6f49177526a7dae412a5db6e783884d6c4
Augusto Lamim <augustocesarlamim@gmail.com>
2022-05-10 10:10:02 -0300

    make it better

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     dialect :'postgres',    
-    host: 'localhost',
-    username: 'postgres',
-    password: 'root',
-    database: 'dev',    
+    host: 'chunee.db.elephantsql.com',
+    username: 'yaxufctb',
+    password: 'TJFf8uXpFW13rxURsTs1NPpq8wq6yxLC',
+    database: 'yaxufctb',    
     port: '5432'  
 };      
